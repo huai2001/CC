f8073a53f6c4f3ef2d1eb14a3a54af8727f9f52b
. Qiu <cc@qiu-2.local>
2026-01-29 22:23:08 +0800

    Update array.h

--- include/libcc/array.h
+++ include/libcc/array.h
@@ -23,6 +23,7 @@ extern "C" {
 typedef uintptr_t _cc_array_t;
 
 #define _cc_array_value(CTX,INDEX)  (*((uintptr_t*)(CTX) + (INDEX)));
+#define _cc_array_at(CTX,INDEX)  (*((uintptr_t*)(CTX) + (INDEX)));
 
 /**/
 _CC_API_PUBLIC(_cc_array_t) _cc_alloc_array(size_t);
